47193e80d1545ee85b86a1aa18bb7c7375c8c267
cleverdog <motoki@cleverdog.me>
2024-02-06 16:11:48 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorWasmApp_IGsample_for_CnG/App.razor.js
@@ -0,0 +1,15 @@
+﻿export function init() {
+
+    $("#barcode").igQRCodeBarcode({
+        height: "300px",
+        width: "100%",
+        data: "http://www.infragistics.com/products/jquery/",
+    });
+
+	$("#spreadsheet").igSpreadsheet({
+		height: "600",
+		width: "100%",
+		isFormulaBarVisible: true,
+	});
+
+}
